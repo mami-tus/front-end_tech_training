fb23490aeca52806846877a2111116477e24d3c9
mami <85916526+mami-tus@users.noreply.github.com>
2022-02-15 22:44:44 +0900

    add text animation

--- js/main.js
+++ js/main.js
@@ -34,3 +34,27 @@ $('.slider-1').slick({
   dots: true,
   arrows: false,
 });
+
+
+// 文字アニメーション
+document.addEventListener('DOMContentLoaded', function () {
+
+  const els = document.querySelectorAll('.animate-title');
+  const cb = function (entries, observer) {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              const ta = new TextAnimation(entry.target);
+              ta.animate();
+              observer.unobserve(entry.target);
+          } else {
+          }
+      });
+  };
+  const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0
+  };
+  const io = new IntersectionObserver(cb, options);
+  els.forEach(el => io.observe(el));
+});
